211da46218a013b3ed6201263d990ca275e2e628
cheipstudio-cmyk <cheipstudio@gmail.com>
2026-05-08 20:06:22 +0200

    v111

--- app/src/main/java/org/cheipstudio/speedlauncher/data/AppInfo.kt
+++ app/src/main/java/org/cheipstudio/speedlauncher/data/AppInfo.kt
@@ -1,13 +1,19 @@
 package org.cheipstudio.speedlauncher.data
 
 import android.graphics.drawable.Drawable
+import android.os.UserHandle
 
+/**
+ * Informazioni su un'app installata visibile nel launcher.
+ * v102: ricreato dopo perdita del file (apparentemente perso in qualche commit precedente).
+ */
 data class AppInfo(
     val packageName: String,
     val componentName: String,
     val label: String,
     val icon: Drawable,
-    val userHandle: android.os.UserHandle
+    val userHandle: UserHandle
 ) {
+    /** Chiave univoca per identificare l'app (pkg/component) */
     val key: String get() = "$packageName/$componentName"
 }
